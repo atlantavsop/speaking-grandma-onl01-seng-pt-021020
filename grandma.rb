4bf6dde6e4968ffd673a8d2be7de57da3037b3d7
Angel Nunez <atlantavsop@gmail.com>
2020-02-18 16:56:38 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,7 +15,9 @@
 def speak_to_grandma(phrase)
   if phrase == 'I LOVE YOU GRANDMA'
     'I LOVE YOU TOO PUMKIN'
-  else
+  elseif phrase != phrase.upcase
     'HUH?! SPEAK UP, SONNY!'
+  else
+    'NO, NOT SINCE 1938!'
   end
 end
